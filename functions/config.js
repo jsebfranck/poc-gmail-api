30909d1656ebf2682ed21f1bc972758f37c826c3
Jean-Sébastien Franck <jsebfranck@gmail.com>
2018-06-08 13:22:10 +0200

    Firebase integration

---
+++ functions/config.js
@@ -0,0 +1,9 @@
+module.exports = config = {
+    gmail: {
+        //redirectBaseUrl: 'http://localhost:5000/gmail/connected?userId=',
+        redirectBaseUrl: 'https://test-61ce9.firebaseapp.com/gmail/connected?userId=',
+        scopes: ['https://www.googleapis.com/auth/gmail.readonly'],
+        client_secret: 'ycI2oxcQqoeuAltd2OsQI2yl',
+        client_id: '182229047738-phfvq02upl62sfas8m9kngdckejgdugr.apps.googleusercontent.com',
+    } 
+}
--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,42 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const {google} = require('googleapis');
+const config = require('./config');
+
+const app = express();
+
+app.get('/gmail/auth', (req, res) => {
+  const userId = req.query.userId || 'unknown';
+  const redirectUrl = config.gmail.redirectBaseUrl + userId;
+  const oAuth2Client = new google.auth.OAuth2(config.gmail.client_id, config.gmail.client_secret, redirectUrl);
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'online',
+    scope: config.gmail.scopes,
+  });
+  res.redirect(authUrl);
+});
+
+app.get('/gmail/connected', (req, res) => {
+  const userId = req.query.userId || 'unknown';
+  const code = req.query.code;
+
+  const redirectUrl = config.gmail.redirectBaseUrl + userId;
+  const auth = new google.auth.OAuth2(config.gmail.client_id, config.gmail.client_secret, redirectUrl);
+  auth.getToken(code, (err, token) => {
+    if (err) res.json({err});
+    auth.setCredentials(token);
+
+    const gmail = google.gmail({version: 'v1', auth});
+    gmail.users.threads.list({
+      userId: 'me',
+    }, (err, {data}) => {
+      if (err) res.json({err});
+      res.json(data);
+    });
+  });
+});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
+exports.app = functions.https.onRequest(app);
